27e48c3c8cf8821079a48974130c33581f5c5949
Nirjhar <choudhurynirjhar@gmail.com>
2019-09-16 19:44:37 -0400

    Autofac Integration

--- Docker.Demo/ContainerConfiguration.cs
+++ Docker.Demo/ContainerConfiguration.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace Docker.Demo
 {
     internal static class ContainerConfiguration
     {
-        public static ServiceProvider Configure()
+        public static IServiceProvider Configure()
         {
-            return new ServiceCollection()
-                .AddLogging(l => l.AddConsole())
-                .Configure<LoggerFilterOptions>(c => c.MinLevel = LogLevel.Trace)
-                .AddSingleton<IPrintSettingsProvider, PrintSettingsProvider>()
-                .AddSingleton<IConsolePrinter, ConsolePrinter>()
-                .AddSingleton<ContinuousRunningProcessor>()
-                .BuildServiceProvider();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(l => l.AddConsole())
+                .Configure<LoggerFilterOptions>(c => c.MinLevel = LogLevel.Trace);
+
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.Populate(serviceCollection);
+
+            containerBuilder.RegisterType<PrintSettingsProvider>().As<IPrintSettingsProvider>().SingleInstance();
+            containerBuilder.RegisterType<ConsolePrinter>().As<IConsolePrinter>().SingleInstance();
+            containerBuilder.RegisterType<ContinuousRunningProcessor>().SingleInstance();
+
+            var container = containerBuilder.Build();
+
+            return new AutofacServiceProvider(container);
         }
     }
 }
